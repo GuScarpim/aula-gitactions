f2bb0e44fd0103cc9627fea0df2b9b9db18486cb
guscarpim <gustavoscarpim@gmail.com>
2025-07-08 20:18:24 -0300

    fix: fix a test

--- App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { test, expect } from 'vitest';
-import App from './App';
-
-test('renders title', () => {
-  render(<App />);
-  expect(screen.getByText(/Vite \+ React/i)).toBeInTheDocument();
-});
